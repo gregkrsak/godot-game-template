2f434aa5fd38b7875ee9b83a77159c3639a1f87b
Greg M. Krsak <greg.krsak@gmail.com>
2022-02-16 10:16:03 -0800

    Fixed command formatting

--- checkout-default-submodule-branches.sh
+++ checkout-default-submodule-branches.sh
@@ -1,1 +1,3 @@
-cd godot-addons-as-submodules/ && git submodule foreach --recursive "../../set-and-checkout-default-submodule-branch.sh" && cd ..
+cd godot-addons-as-submodules/
+git submodule foreach --recursive "../../set-and-checkout-default-submodule-branch.sh"
+cd ..
