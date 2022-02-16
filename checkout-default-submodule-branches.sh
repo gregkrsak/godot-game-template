9ec78d129e800b9ea7644ed77d33465990416767
Greg M. Krsak <greg.krsak@gmail.com>
2022-02-16 14:41:58 -0800

    Renamed subcommand to have leading underscore

--- checkout-default-submodule-branches.sh
+++ checkout-default-submodule-branches.sh
@@ -1,4 +1,4 @@
 echo "Setting addon submodule branches to their remote defaults. Please wait..."
 cd godot-addons-as-submodules/
-git submodule foreach --recursive "../../set-and-checkout-default-submodule-branch.sh"
+git submodule foreach --recursive "../../_set-and-checkout-default-submodule-branch.sh"
 cd ..
