3eeb64892c286927e8e932bfad8a2b95ecb6c226
Greg M. Krsak <greg.krsak@gmail.com>
2022-02-15 21:30:13 -0800

    Updated reset script

--- reset-submodules.sh
+++ reset-submodules.sh
@@ -4,4 +4,15 @@ rm -rf .git/modules/godot-addons-as-submodules/
 rm -rf godot-addons-as-submodules
 git submodule sync
 git submodule update
+
+echo ""
+echo "Switching away from detatched HEAD mode..."
+git submodule--helper set-branch --branch main godot-addons-as-submodules
+cd godot-addons-as-submodules
+git branch -u origin/main main
+git checkout main
+cd ..
+
+echo ""
+
 ./update-submodules.sh
