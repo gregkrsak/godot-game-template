61509b3982fb583cde96c7da075e66fc301995fc
Greg M. Krsak <greg.krsak@gmail.com>
2022-02-16 14:40:20 -0800

    Renamed subcommand to have leading underscore

--- set-and-checkout-default-submodule-branch.sh
+++ set-and-checkout-default-submodule-branch.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-declare -x BRANCH=$(../../show-submodule-branch.sh)
+declare -x BRANCH=$(../../_show-submodule-branch.sh)
 echo "Remote default branch is '${BRANCH}'..."
 git branch -u origin/${BRANCH} ${BRANCH}
 git checkout ${BRANCH}
