a9f793556acd99b4989db07db2855bf5c558474a
Greg M. Krsak <greg.krsak@gmail.com>
2022-02-15 20:14:25 -0800

    Added web reference

--- reset-submodules.sh
+++ reset-submodules.sh
@@ -1,3 +1,4 @@
+echo "Reference: https://stackoverflow.com/questions/14404704/how-do-i-replace-a-git-submodule-with-another-repo"
 echo "Resetting submodules. Please wait..."
 rm -rf .git/modules/godot-addons-as-submodules/
 rm -rf godot-addons-as-submodules
